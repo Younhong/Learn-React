5ac875440712a0cef9caf25ba27482fc803c6c9b
younhong <younhong@kakao.com>
2020-10-27 19:34:22 +0900

    update

--- src/components/TOC.js
+++ src/components/TOC.js
@@ -8,7 +8,14 @@ class TOC extends Component {
         while (i < data.length) {
             lists.push(
                 <li key={data[i].id}>
-                    <a href={"/content/"+data[i].id}>
+                    <a 
+                    href={"/content/"+data[i].id}
+                    data-id={data[i].id}
+                    onClick={function(e){
+                        e.preventDefault();
+                        this.props.onChangePage(e.target.dataset.id);
+                    }.bind(this)}
+                    >
                         {data[i].title}</a></li>)
             i = i + 1;
         }
